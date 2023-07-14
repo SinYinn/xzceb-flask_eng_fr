feacb8216ac972c99be69136958df17e0874ec07
SinYinn <waisintin2002@gmail.com>
2023-07-14 04:36:39 -0400

    Updated files with changes

--- final_project/server.py
+++ final_project/server.py
@@ -13,12 +13,12 @@ def englishToFrench():
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     text_to_translate = request.args.get('text_to_translate')
-    english_text = french_to_text(text+to_translate)
+    english_text = french_to_text(text_to_translate)
     return english_text
 
 @app.route("/")
 def renderIndexPage():
-    return render.template("index.html")
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
